108752479a980ecf5f47f1cdc237f2cea17d53d0
nguyenleduy1996 <104442210+nguyenleduy1996@users.noreply.github.com>
2025-09-14 23:29:40 +0700

    Dichuyen 4 Huong
    Dichuyen 4 Huong

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,38 +1,63 @@
 using UnityEngine;
-using UnityEngine.InputSystem; // để dùng Input System mới
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;         // tốc độ di chuyển
-    private Vector2 moveInput;           // input từ bàn phím
-    private Rigidbody2D rb;              // rigidbody để di chuyển
+    public float moveSpeed = 3f;
 
-    private PlayerControls controls;     // class auto-gen từ PlayerControls.inputactions
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 moveInput;
+
+    private HuongNhanVat _huongMatNhanVat;
+    private bool _isMoving;
+
+    private InputAction moveAction;
+
+    public enum HuongNhanVat { Trai = 1, Phai = 2, Len = 3, Xuong = 4 }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        controls = new PlayerControls(); // tạo instance
+        animator = GetComponent<Animator>();
+
+        moveAction = new InputAction("Move", InputActionType.Value);
+        moveAction.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/w")
+            .With("Down", "<Keyboard>/s")
+            .With("Left", "<Keyboard>/a")
+            .With("Right", "<Keyboard>/d");
     }
 
-    private void OnEnable()
-    {
-        // Enable input
-        controls.Enable();
+    private void OnEnable() => moveAction.Enable();
+    private void OnDisable() => moveAction.Disable();
 
-        // Lắng nghe action Move
-        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
-    }
-
-    private void OnDisable()
+    private void Update()
     {
-        controls.Disable();
+        moveInput = moveAction.ReadValue<Vector2>();
+        _isMoving = moveInput != Vector2.zero;
+
+        if (_isMoving) //Huong Nhan Vat
+        {
+            if (Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
+                _huongMatNhanVat = moveInput.x > 0 ? HuongNhanVat.Phai : HuongNhanVat.Trai;
+            else
+                _huongMatNhanVat = moveInput.y > 0 ? HuongNhanVat.Len : HuongNhanVat.Xuong;
+        }
+
+        UpdateAnimation();
     }
 
     private void FixedUpdate()
     {
-        // Di chuyển player theo input (RPG top-down nên bỏ gravity)
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void UpdateAnimation()
+    {
+        animator.SetInteger("HuongNhanVat", (int)_huongMatNhanVat);
+        animator.SetBool("IsMoving", _isMoving);
     }
 }
